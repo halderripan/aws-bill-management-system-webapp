1c44c43144053c9f326dc911ce7437565349b1ca
“Ripan <halder.r@husky.neu.edu>
2020-03-17 03:06:11 -0400

    Cloudwatch initial dummy config test

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,3 +3,9 @@ sudo npm install forever -g
 sudo npm install pm2 -g
 cd /home/ubuntu/
 sudo npm install
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:/opt/cloudwatch-config.json \
+    -s
